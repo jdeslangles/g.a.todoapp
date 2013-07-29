909ba99d11ade43ac2480a563b0f0a7cc5949fa4
jdeslangles <julien.deslangles@gmail.com>
2013-07-29 13:10:12 +0100

    Finally - draggable functionality back on

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,35 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
 //= require jquery_ujs
 //= require_tree .
 
-
-$(function() {
-    $('.sortable').sortable();
-    $('.sortable').disableSelection();
-
-    $('#list li').draggable({
-      appendTo: "body",
-      revert: "invalid",
-      cursor: "move",
-      helper: "clone"
-    });
-
-    $('#list ul').droppable({
-      activeClass: "ui-state-default",
-      hoverClass:"ui-state-hover",
-      accept:":not(.ui-sortable-helper)",
-      drop:function(event,ui){
-        $(this).find(".placeholder").remove();
-        $("<li></li>").text(ui.draggable.text()).appendTo(this); }
-      }).sortable({
-        items:"li:not(.placeholder)",
-        sort:function(){
-          $( this).removeClass("ui-state-default");
-    })
-
-  })
-});
 
---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,26 @@
+$(function() {
+    $('.sortable').sortable();
+    $('.sortable').disableSelection();
+
+    $('#list li').draggable({
+      appendTo: "body",
+      revert: "invalid",
+      cursor: "move",
+      helper: "clone"
+    });
+
+    $('#list ul').droppable({
+      activeClass: "ui-state-default",
+      hoverClass:"ui-state-hover",
+      accept:":not(.ui-sortable-helper)",
+      drop:function(event,ui){
+        $(this).find(".placeholder").remove();
+        $("<li></li>").text(ui.draggable.text()).appendTo(this); }
+      }).sortable({
+        items:"li:not(.placeholder)",
+        sort:function(){
+          $( this).removeClass("ui-state-default");
+        }
+      })
+});
+
